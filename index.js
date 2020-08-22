753f36d0154ae1e9f530c7bd9a6436b14b96b2f3
technologicalsouls <technologicalsouls@gmail.com>
2020-08-22 11:27:46 -0400

    add more functionalities to timer class

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
-// FIRST STAGE -- SET UP TIMER CLASS ONLY -- FIGURE THIS ONE COMPONENT OUT
+//  ADDING IN THE STOP / START/ PAUSE FUNCTIONALITY AND HOW TO EXPRESS THAT TO THE USER -- ALERTING / GIVING FEEDBACK TO USER WHEN SOMETHING IMPORTANT HAPPENED TO THE 'MECHANICS' OF THE TIMER.
+
+//also made TIMER class into it's own file -- timer.js (deleted class timer here to clean file )
 
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if (callbacks) {
+            // if there is an onStart callback
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
-    // arrow function solve "this" problem
     start = () => {
-        // invoke tick at start to start ticking AT CLICK
+// chieck if there was an onstart callback already
+        if (this.onStart) {
+            this.onStart();
+        }
         this.tick();
-        //setting interval
         this.interval = setInterval(this.tick, 1000);
-    };
+    }
 
     pause = () => {
         clearInterval(this.interval);
     }
-    // run once every second
+
     tick = () => {
-        //to check when timer stops aka 0
         if (this.timeRemaining <= 0) {
             this.pause();
+            if (this.onComplete) [
+                this.onComplete()
+            ]
         } else {
-        //old way -- remember to convert to number from string (since value is a string)
-            // const timeRemaining = parseFloat(this.durationInput.value);
-        //new way - use getter ( timeRemaining a class method for internal class stuff )
-        // //update the duration to reflect now
-        // OLD WAY w/o setter
-            // this.durationInput.value = timeRemaining - 1;
-            this.timeRemaining= this.timeRemaining - 1;
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick();
+            }
         }
-    };
+    }
 
-    // use 'get' keyword before function (getter) - allows to retrieve var out of a class w/o creating a new method
     get timeRemaining() {
         return parseFloat(this.durationInput.value);
     }
 
     set timeRemaining(time) {
-        // this.durationInput.value = time;
+        this.durationInput.value = time;
     }
 }
 
@@ -51,5 +58,17 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+//optional 4th argument
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    //series of diff callbacks -- invoked at specific times - for user feedback
+    onStart() {
+        console.log('timer started');
+    },
+    onTick() {
+        console.log('timer just ticked down');
+    },
+    onComplete() {
+        console.log('timer done');
+    }
+});
 
